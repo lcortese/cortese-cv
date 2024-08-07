1557607071c6b8dac84363a06685008c29329849
Leandro Cortese <leandrocortese@gmail.com>
2024-08-06 22:24:59 -0300

    Install prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 const config = [
   {
@@ -25,6 +26,7 @@ const config = [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  eslintConfigPrettier
 ];
 
 export default config;
